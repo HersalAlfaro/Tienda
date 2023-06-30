9254bc640bbd6487fff857f21e3e08cc6d7bfefc
hersa <hersa@DESKTOP-0EQLH6M>
2023-06-29 21:00:15 -0600

    sexto commit tienda

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,25 @@
+
+package com.tienda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+    
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String bucketName = "techshop-37859.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-37859-firebase-adminsdk-mnzr7-e25c0682a6.json";
+    
+}
+
